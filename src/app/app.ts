3296ee21c30f300bb552ad9fe1e64acbb1b58cc4
Felipe Solis Dominguez <felipe.solis@usach.cl>
2026-03-30 18:34:50 -0300

    ADD: manejo para crear un usuario
    Implementacion ayuda a incorporar un nuevo usuario al arreglo ya definido

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { UserCardComponent } from './components/user-card/user-card';
 import { User } from './services/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserCardComponent],
+  imports: [RouterOutlet, UserCardComponent, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -15,4 +16,16 @@ export class App {
   private userService = inject(User);
 
   users = this.userService.users;
+
+  newUserName = signal('');
+  newUserRole = signal('');
+
+  saveUser() {
+    if (this.newUserName().trim() && this.newUserRole().trim()) {
+      this.userService.addUser(this.newUserName(), this.newUserRole());
+
+      this.newUserName.set('');
+      this.newUserRole.set('');
+    }
+  }
 }
