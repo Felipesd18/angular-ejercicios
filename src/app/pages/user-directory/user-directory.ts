09c8cc5671e419aed2898d5f0a732085988dfa4f
Felipe Solis Dominguez <felipe.solis@usach.cl>
2026-04-04 19:22:20 -0300

    ADD: manejo formulario Angular
    Manejo formulario reactivo

--- src/app/pages/user-directory/user-directory.ts
+++ src/app/pages/user-directory/user-directory.ts
@@ -1,11 +1,11 @@
 import { Component, signal, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserCardComponent } from '../../components/user-card/user-card';
 import { User } from '../../services/user';
 
 @Component({
   selector: 'app-user-directory',
-  imports: [FormsModule, UserCardComponent],
+  imports: [ReactiveFormsModule, UserCardComponent],
   templateUrl: './user-directory.html',
   styleUrl: './user-directory.css',
 })
@@ -14,15 +14,21 @@ export class UserDirectory {
 
   users = this.userService.users;
 
-  newUserName = signal('');
-  newUserRole = signal('');
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    role: new FormControl('', [Validators.required]),
+  });
 
   saveUser() {
-    if (this.newUserName().trim() && this.newUserRole().trim()) {
-      this.userService.addUser(this.newUserName(), this.newUserRole());
+    if (this.userForm.valid) {
+      const newName = this.userForm.value.name ?? '';
+      const newRole = this.userForm.value.role ?? '';
 
-      this.newUserName.set('');
-      this.newUserRole.set('');
+      this.userService.addUser(newName, newRole);
+
+      this.userForm.reset();
+    } else {
+      this.userForm.markAllAsTouched();
     }
   }
 }
